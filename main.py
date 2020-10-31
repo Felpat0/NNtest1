928d7440094a8a8b92e39a6e72e86347793a18ce
Felpat0 <b994@live.com>
2020-10-31 13:45:04 +0100

    Created features layer

--- main.py
+++ main.py
@@ -29,3 +29,44 @@ test_df_std = test_df.std()
 test_df_norm = (test_df - test_df_mean)/test_df_std
 
 print("Normalized the values.")
+
+# Create an empty list that will eventually hold all created feature columns.
+feature_columns = []
+
+# We scaled all the columns, including latitude and longitude, into their
+# Z scores. So, instead of picking a resolution in degrees, we're going
+# to use resolution_in_Zs.  A resolution_in_Zs of 1 corresponds to 
+# a full standard deviation. 
+resolution_in_Zs = 0.3  # 3/10 of a standard deviation.
+
+# Create a bucket feature column for latitude.
+latitude_as_a_numeric_column = tf.feature_column.numeric_column("latitude")
+latitude_boundaries = list(np.arange(int(min(train_df_norm['latitude'])), 
+                                     int(max(train_df_norm['latitude'])), 
+                                     resolution_in_Zs))
+latitude = tf.feature_column.bucketized_column(latitude_as_a_numeric_column, latitude_boundaries)
+
+# Create a bucket feature column for longitude.
+longitude_as_a_numeric_column = tf.feature_column.numeric_column("longitude")
+longitude_boundaries = list(np.arange(int(min(train_df_norm['longitude'])), 
+                                      int(max(train_df_norm['longitude'])), 
+                                      resolution_in_Zs))
+longitude = tf.feature_column.bucketized_column(longitude_as_a_numeric_column, 
+                                                longitude_boundaries)
+
+# Create a feature cross of latitude and longitude.
+latitude_x_longitude = tf.feature_column.crossed_column([latitude, longitude], hash_bucket_size=100)
+crossed_feature = tf.feature_column.indicator_column(latitude_x_longitude)
+feature_columns.append(crossed_feature)  
+
+# Represent median_income as a floating-point value.
+median_income = tf.feature_column.numeric_column("median_income")
+feature_columns.append(median_income)
+
+# Represent population as a floating-point value.
+population = tf.feature_column.numeric_column("population")
+feature_columns.append(population)
+
+# Convert the list of feature columns into a layer that will later be fed into
+# the model. 
+my_feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
