c8fe3021b95243b935ce3c88879dc806a158475b
Felpat0 <b994@live.com>
2020-10-31 13:42:09 +0100

    Normalized dfs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#@title Run on TensorFlow 2.x
 %tensorflow_version 2.x
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -18,3 +17,15 @@ print("Imported modules.")
 train_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
 train_df = train_df.reindex(np.random.permutation(train_df.index)) # shuffle the examples
 test_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_test.csv")
+
+# Calculate the Z-scores of each column in the training set:
+train_df_mean = train_df.mean()
+train_df_std = train_df.std()
+train_df_norm = (train_df - train_df_mean)/train_df_std
+
+# Calculate the Z-scores of each column in the test set.
+test_df_mean = test_df.mean()
+test_df_std = test_df.std()
+test_df_norm = (test_df - test_df_mean)/test_df_std
+
+print("Normalized the values.")
